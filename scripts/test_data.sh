07f367f89ce97d7bfc2dacdc1bd3cd1706ea72f1
TanellD <128409451+TanellD@users.noreply.github.com>
2024-07-02 20:08:03 +0300

    Versioned data sample: data/samples/sample.csv

--- scripts/test_data.sh
+++ scripts/test_data.sh
@@ -4,17 +4,17 @@
 set -e
 
 # Define paths
-SAMPLE_SCRIPT="src.data"
 SAMPLE_FUNCTION="sample_data"
 SAMPLE_FILE="data/samples/sample.csv"
 TESTS_DIR="tests"
 
+
+export PYTHONPATH="$PWD/src"
+
+
 # Step 1: Take a data sample using sample_data() from data.py
 echo "Taking a data sample..."
-python -c "
-from $SAMPLE_SCRIPT import $SAMPLE_FUNCTION
-$SAMPLE_FUNCTION()
-"
+python $PYTHONPATH/data.py
 
 # Step 2: Validate the data sample by running pytest
 echo "Validating the data sample..."
@@ -23,13 +23,17 @@ TEST_STATUS=$?
 
 # Step 3: Version the data sample using DVC if tests pass
 if [ $TEST_STATUS -eq 0 ]; then
+    DATA_VERSION=$( grep -A3 'data_version:' configs/config.yaml | tail -n1 | awk '{ print $2}' )
     echo "Tests passed. Versioning the data sample..."
-
     # Add the versioned sample file to DVC and commit
     dvc add $SAMPLE_FILE
     git add .
     git commit -m "Versioned data sample: $SAMPLE_FILE"
+    git push
+    git tag -a "v$DATA_VERSION" -m "add data version v$DATA_VERSION"
+    git push --tags
     dvc push
+    python change_config.py
 else
     echo "Tests failed. Data sample will not be versioned."
     exit 1
--- src/data.py
+++ src/data.py
@@ -8,12 +8,17 @@ def sample_data(cfg: DictConfig):
     data_url = cfg.data.url
     sample_size = cfg.data.sample_size
     sample_file = cfg.data.sample_file
+    version = cfg.data.data_version
 
     # Read the data
     df = pd.read_csv(data_url)
 
     # Sample the data
-    sample_df = df.sample(frac=sample_size, random_state=42)
+    start = int((version - 1)*(sample_size*len(df)))
+    end = int(version*(sample_size*len(df)))
+    if version == 5.0:
+        end = len(df)
+    sample_df = df.iloc[start:end]
 
     # Ensure the samples directory exists
     os.makedirs(os.path.dirname(sample_file), exist_ok=True)
